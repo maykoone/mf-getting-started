3449768f794d2dd828ec5a6f5be7d869c84a8ca0
Mayko B. Oliveira <maykoone@gmail.com>
2024-02-24 15:04:12 -0300

    Configuring module federation on application A
    Configuration to expose component SayHelloFromA via webpack's
    module federation plugin

--- packages/application-a/webpack.config.js
+++ packages/application-a/webpack.config.js
@@ -1,10 +1,14 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require("webpack").container;
 
 const mode = process.env.NODE_ENV || 'production';
 
 module.exports = {
   mode,
   entry: './src/index',
+  output: {
+    publicPath: 'http://localhost:3001/',
+  },
   devtool: 'source-map',
   optimization: {
     minimize: mode === 'production',
@@ -25,6 +29,15 @@ module.exports = {
   },
 
   plugins: [
+    new ModuleFederationPlugin({
+      name: 'application_a',
+      library: { type: 'var', name: 'application_a' },
+      filename: 'remoteEntry.js',
+      exposes: {
+        './SayHelloFromA': './src/app',
+      },
+      shared: ['react', 'react-dom'],
+    }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
